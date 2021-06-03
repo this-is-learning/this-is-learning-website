759a00fd051bb004909dff5452e4348b6d046adf
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-03 17:37:19 +0200

    chore: add Scully

--- apps/this-is-learning/src/app/app.module.ts
+++ apps/this-is-learning/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent, AppScam } from './app.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppScam, AppRoutingModule],
+  imports: [BrowserModule, AppScam, AppRoutingModule, ScullyLibModule],
 })
 export class AppModule {}
---
+++ scully.this-is-learning.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./apps/this-is-learning/src",
+  projectName: "this-is-learning",
+  outDir: './dist/static',
+  routes: {
+  }
+};
