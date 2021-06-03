a04a0b4fe57d947df5d5a3ba9cf86c12d0188a42
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-03 16:38:00 +0200

    ci: add CI workflow (#2)
    * chore: migrate to workspace schema v2
    
    * chore: add Nx Cloud, configure tasks
    
    * ci: add CI workflow

--- apps/this-is-learning-e2e/src/integration/app.spec.ts
+++ apps/this-is-learning-e2e/src/integration/app.spec.ts
@@ -5,7 +5,7 @@ describe('this-is-learning', () => {
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    // Example: cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('Welcome to this-is-learning!');
--- apps/this-is-learning/src/app/app.component.ts
+++ apps/this-is-learning/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'this-is-learning-root',
+  selector: 'til-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
