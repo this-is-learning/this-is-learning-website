db6f07f75844e7af80e1491415431e0a006856eb
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-03 18:05:29 +0200

    test: fix component test

--- apps/this-is-learning/src/app/app.component.spec.ts
+++ apps/this-is-learning/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent, AppScam } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppScam],
+      imports: [AppScam, RouterTestingModule],
     }).compileComponents();
   });
 
