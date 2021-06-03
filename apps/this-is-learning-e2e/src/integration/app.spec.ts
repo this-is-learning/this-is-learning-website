d861289ea4dcaa5e7c1e8b5cfd631f6b8a45ad68
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-03 18:06:57 +0200

    test: fix e2e test

--- apps/this-is-learning-e2e/src/integration/app.spec.ts
+++ apps/this-is-learning-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('this-is-learning', () => {
     // Example: cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to this-is-learning!');
+    getGreeting().contains('Hello, TiL!');
   });
 });
