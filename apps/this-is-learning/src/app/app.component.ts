dda08f513cece32732951ffe9af3df64fae93b79
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-03 18:05:47 +0200

    style: format

--- apps/this-is-learning/src/app/app.component.ts
+++ apps/this-is-learning/src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, NgModule, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  NgModule,
+  ViewEncapsulation,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
--- apps/this-is-learning/src/app/hello.component.ts
+++ apps/this-is-learning/src/app/hello.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, NgModule, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  NgModule,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
