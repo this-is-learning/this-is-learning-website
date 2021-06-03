eab133282d2515a34e09d855744c3e2bc21aad43
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-03 17:14:29 +0200

    chore: remove Angular Universal (#4)

--- apps/this-is-learning/src/app/app.module.ts
+++ apps/this-is-learning/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
