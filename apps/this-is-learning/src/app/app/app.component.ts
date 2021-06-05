c783a5b8920b3d1abef622038c5bfe1960dc1226
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-06 00:13:16 +0200

    style: format

--- apps/this-is-learning/src/app/app/app.component.ts
+++ apps/this-is-learning/src/app/app/app.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
