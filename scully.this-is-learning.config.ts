2084888908021e49d36149fc06d95f08565bae21
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-06 22:40:49 +0200

    chore: disable Scully routing

--- scully.this-is-learning.config.ts
+++ scully.this-is-learning.config.ts
@@ -1,19 +1,17 @@
 import { ScullyConfig } from '@scullyio/scully';
 
-import { fromRss } from './scully/plugins/from-rss.plugin';
-
 export const config: ScullyConfig = {
   projectRoot: './apps/this-is-learning/src',
   projectName: 'this-is-learning',
   outDir: './dist/static/this-is-learning',
   routes: {
-    '/this-is-angular': {
-      rss: 'https://dev.to/feed/this-is-angular',
-      type: fromRss,
-    },
-    '/this-is-learning': {
-      rss: 'https://dev.to/feed/this-is-learning',
-      type: fromRss,
-    },
+    // '/this-is-angular': {
+    //   rss: 'https://dev.to/feed/this-is-angular',
+    //   type: fromRss,
+    // },
+    // '/this-is-learning': {
+    //   rss: 'https://dev.to/feed/this-is-learning',
+    //   type: fromRss,
+    // },
   },
 };
