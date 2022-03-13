a4cf7269e764c9bb2355d9e84bbb37e1f5e510cf
haroon786 <haroon.ansari123@gmail.com>
2022-03-13 18:34:03 +0530

    reduced css

--- apps/this-is-learning/src/app/app.module.ts
+++ apps/this-is-learning/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ScullyLibModule } from '@scullyio/ng-lib';
-import { NavigationModule } from '@this-is-learning/navigation';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,7 +8,7 @@ import { HomeComponent } from './home/home.component';
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ScullyLibModule,NavigationModule],
+  imports: [BrowserModule, AppRoutingModule, ScullyLibModule],
   declarations: [
     AppComponent, HomeComponent
   ],
